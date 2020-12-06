66310e82f43fa85c4f653d6cfaa71ea7e3d29c32
Christian Meyer <christian.meyer@gmail.com>
2020-12-06 21:07:56 +0100

    getting ready for new release

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("com.github.ben-manes.versions") version "0.36.0"
 }
 
-version = "0.0.2-SNAPSHOT"
+version = "0.0.2"
 group = "xyz.chrisime"
 description = "CRooD (an easy-to-use CRUD Repository based on jOOQ)"
 
