4801c91de9a908a6f3b519f3fcc8c8a11cc42abc
Christian Meyer <christian.meyer@gmail.com>
2020-12-26 12:34:30 +0100

    removed test domain related generator/strategy/annotations

--- src/main/kotlin/xyz/chrisime/crood/codegen/annotation/ForeignKey.kt
+++
@@ -1,6 +0,0 @@
-package xyz.chrisime.crood.codegen.annotation
-
-@MustBeDocumented
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FIELD)
-annotation class ForeignKey(val refColumnName: String = "id", val columnName: String, val isNullable: Boolean = false)
--- src/main/kotlin/xyz/chrisime/crood/codegen/annotation/Size.kt
+++
@@ -1,6 +0,0 @@
-package xyz.chrisime.crood.codegen.annotation
-
-@MustBeDocumented
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FIELD)
-annotation class Size(val min: Int = 0, val max: Int = Integer.MAX_VALUE)
