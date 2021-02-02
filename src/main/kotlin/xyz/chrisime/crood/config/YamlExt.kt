ce803ffbe8e8518d11fe9771728a2dcf1a1cd7d4
Christian Meyer <code@chrisime.xyz>
2021-02-02 23:17:56 +0100

    find config file iterating list of allowed file names

--- src/main/kotlin/xyz/chrisime/crood/config/YamlExt.kt
+++ src/main/kotlin/xyz/chrisime/crood/config/YamlExt.kt
@@ -5,16 +5,20 @@ import org.yaml.snakeyaml.constructor.Constructor
 import org.yaml.snakeyaml.introspector.BeanAccess
 import java.io.File
 
-object Configuration {
-    private val ymlFile: File by lazy { File("src/main/resources/crood.yml") }
-    private val yamlFile: File by lazy { File("src/main/resources/crood.yaml") }
-
-    fun getConfig(): CRooDYaml {
-        return ymlFile.parseYaml<CRooDYaml>() ?: yamlFile.parseYaml<CRooDYaml>() ?: throw IllegalArgumentException()
-    }
+private val configFiles: List<File> by lazy {
+    listOf(
+        File("src/main/resources/crood.yml"),
+        File("src/main/resources/crood.yaml")
+    )
 }
 
-inline fun <reified T> File.parseYaml(): T? {
+val croodConfig = configFiles.map {
+    it.parseYaml<CRooDYaml>()
+}.first {
+    it != null
+} ?: throw IllegalArgumentException()
+
+private inline fun <reified T> File.parseYaml(): T? {
     val inputStream = ClassLoader.getSystemResourceAsStream(name) ?: return null
 
     return inputStream.bufferedReader().use { bis ->
@@ -24,4 +28,3 @@ inline fun <reified T> File.parseYaml(): T? {
         }
     }
 }
-
