501809dc909dd00248e8241c0605cbf03a6dde8f
Christian Meyer <code@chrisime.xyz>
2022-10-30 15:25:57 -0300

    disable jar creation

--- examples/spring-boot/build.gradle.kts
+++ examples/spring-boot/build.gradle.kts
@@ -61,6 +61,10 @@ tasks {
         }
     }
 
+    jar {
+        enabled = false
+    }
+
     java {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
